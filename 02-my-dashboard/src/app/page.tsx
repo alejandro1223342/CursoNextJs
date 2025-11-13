03e775ea1ce74dd8cc380f21eccdae25340e0af5
Alejoss <alejomuoss@gmail.com>
2025-11-13 11:55:11 -0500

    Estructura del dashboard

---
+++ 02-my-dashboard/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { redirect } from "next/navigation";
+import Image from "next/image";
+
+export default function Home() {
+
+  redirect('/dashboard/counter')
+
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+    </>
+  );
+}
