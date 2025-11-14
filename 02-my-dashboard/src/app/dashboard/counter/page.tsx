870fc76895d77e20258006a229c08168148473a6
Alejoss <alejomuoss@gmail.com>
2025-11-13 20:50:33 -0500

    Pequeños componentes

--- 02-my-dashboard/src/app/dashboard/counter/page.tsx
+++ 02-my-dashboard/src/app/dashboard/counter/page.tsx
@@ -1,8 +1,19 @@
+import CartCounter from "../../shopping-cart/components/CartCounter";
+
+
+export const metadata = {
+  title: 'Counter Page',
+  description: 'Un simple Contador'
+}
 
 export default function NamePage() {
+
+
   return (
-    <div>
-      <h1>Page Counter</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <CartCounter value={20}/>
+
     </div>
   );
 }
